0279512ac615f842b0da29bbb83eba7192d9a592
Anna Sanderson <anna.sanderson@csuglobal.edu>
2019-06-07 22:20:09 -0400

    Added preliminary code for solution

--- app.js
+++ app.js
@@ -23,21 +23,49 @@ function onReady() {
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
+      newLi.classList.add('mdl-list__item', 'newMdl');
+
+      let toDoContainer = document.createElement('span');
+      toDoContainer.classList.add('mdl-list__item-primary-content', 'newMdl');
+
+      let toDoLabel = document.createElement('label');
+      toDoLabel.classList.add('mdl-checkbox', 'mdl-js-checkbox', 'mdl-js-ripple-effect', 'newMdl');
+      toDoLabel.htmlFor = 'list-checkbox-1';
+      
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
-      const deleteBtn = document.createElement('button')
-      deleteBtn.innerHTML = '<span>Delete</span>';
+      checkbox.id = 'list-checkbox-1';
+      checkbox.classList.add('mdl-checkbox__input', 'newMdl');
 
-      newLi.innerHTML = toDo.title;
+      //newLi.innerHTML = toDo.title;
+      let labelText = document.createTextNode(toDo.title);
 
+
+      let deleteContainer = document.createElement('span');
+      deleteContainer.classList.add('mdl-list__item-secondary-action', 'newMdl');
+      const deleteBtn = document.createElement('button');
+      deleteBtn.classList.add('mdl-button', 'mdl-js-button', 'mdl-button--icon', 'newMdl');
+      let icon = document.createElement('i');
+      let text = document.createTextNode('delete');
+      icon.classList.add('material-icons');
+      icon.appendChild(text);
+      deleteBtn.appendChild(icon);
+
+      deleteContainer.appendChild(deleteBtn);
+      toDoLabel.appendChild(checkbox);
+      newLi.appendChild(toDoContainer);
+      toDoContainer.appendChild(toDoLabel);
+      toDoContainer.appendChild(labelText);
       toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-      newLi.appendChild(deleteBtn);
+      newLi.appendChild(deleteContainer);
+
 
       deleteBtn.addEventListener('click', () => {
         toDos = deleteToDo(toDo.id);
         renderTheUI();
       });
+       let mdlElemets = document.querySelectorAll(".newMdl");
+        componentHandler.upgradeElements(mdlElemets);
     });
   }
 
