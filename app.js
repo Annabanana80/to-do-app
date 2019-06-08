bbcd898348fbcb18af4ad707a7b2f4657b51f19e
Anna Sanderson <anna.sanderson@csuglobal.edu>
2019-06-07 20:51:46 -0400

    added code for part 2

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+function onReady() {
+
+  let toDos = [];
+  let id = 0;
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+
+  function createNewToDo() {
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id++
+    });
+  }
+
+  function deleteToDo(id) {
+    return toDos.filter(toDo => toDo.id !== id);
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+      const deleteBtn = document.createElement('button')
+      deleteBtn.innerHTML = '<span>Delete</span>';
+
+      newLi.innerHTML = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteBtn);
+
+      deleteBtn.addEventListener('click', () => {
+        toDos = deleteToDo(toDo.id);
+        renderTheUI();
+      });
+    });
+  }
+
+  addToDoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
+    renderTheUI();
+  });
+
+  renderTheUI();
+}
+
 window.onload = function() {
-   alert("The window has loaded!");
-};
+  onReady();
+}
